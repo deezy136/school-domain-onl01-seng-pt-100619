bb9aa48f17585095da5a86399e42341e42eac5bc
deezy13 <deezy13@gmail.com>
2019-10-24 03:57:56 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,5 +1,6 @@
 # code here!
 class School
+  attr_accessor :name, :grade
   
   roster = {}
   
@@ -7,7 +8,13 @@ class School
     
   end
   
-  
+  def add_student=(name, grade)
+    @name = name 
+    @grade = grade
+    
+    
+    
+  end 
   
   
   
